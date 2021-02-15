d596c10b437e5ba25afdd8751a52ed02ef10c565
Venkatesh0625 <venkatesan.0625@gmail.com>
2021-02-15 15:31:33 +0530

    level 1 anamolies found

--- main.py
+++ main.py
@@ -19,15 +19,14 @@ class Frame:
         self.id = Frame.number_of_frames + 1
         self.image = _img
         self.size = self.image.shape
+# class Anamoly:
+#     def __init__(self, id, row, col):
+#         self.id = id
+#         self.row = row
+#         self.col = col
 
-class Die:
-    number_of_dies = 0
-    def __init__(self, _img):
-        Die.number_of_dies += 1
-        self.id = Die.number_of_dies
-        self.img = _img
-        self.size = img.shape()
-
+#     def calculate_anamoly(height, row, col, die_size, frame_size, street_size):
+#         r = height*frame_size[0] + 
 class Wafer:
     def __init__(self):
         self.data = None
@@ -37,21 +36,22 @@ class Wafer:
 i = 1
 #Load input data
 data = load_input('Level_1_data/input.json')
-im = load_image(f'Level_1_data/wafer_image_1.png')
-dimension_of_frame = im.shape
+dimension_of_frame = load_image(f'Level_1_data/wafer_image_1.png').shape
+
 print(dimension_of_frame)
 
 #Dimension of Wafer
-dimension_of_wafer = (data["die"]["width"]*data["die"]["columns"], data["die"]["height"]*data["die"]["rows"])
+die_size = (data["die"]["width"], data["die"]["height"])
+dimension_of_wafer = (data["die"]["width"]*data["die"]["columns"] + data["street_width"] * (data["die"]["columns"] - 1)
+                        , data["die"]["height"]*data["die"]["rows"] + data["street_width"] * (data["die"]["rows"] - 1))
 print(dimension_of_wafer)
 Wafer_frame_dimn = (dimension_of_wafer[0]/dimension_of_frame[0], dimension_of_wafer[1]/dimension_of_frame[1])
 
 #Number of images in the file
 number_of_image = get_number_images('Level_1_data')
 
-#Main datasets
+#Main dataanamolys
 frame_list = []
-die_list = []
 
 #Standard dev
 std_dev = []
@@ -60,7 +60,7 @@ std_dev = []
 mean_value = []
 
 #Anamoly
-anamoly = []
+anamoly = set()
 
 #wafer
 wafer = load_image(f'Level_1_data/wafer_image_1.png')
@@ -70,37 +70,39 @@ print("\n\n\n",wafer)
 for i in range(2, number_of_image+1):
     img = load_image(f'Level_1_data/wafer_image_{i}.png')
     img = np.reshape(img, (1, dimension_of_frame[0], dimension_of_frame[1]))
-    #inst = Frame(img)
-    #frame_list.append(inst)
     wafer = np.append(wafer, img, axis=0)
 
 wafer_size = wafer.shape
 frame_size = (wafer_size[1], wafer_size[2])
+street_size = data["street_width"]
 
 print(wafer_size, frame_size)
-#print(wafer)
-#Calculate upper and lower value using standard deviation
-upper = np.zeros(frame_size, dtype = int)
-lower =  np.zeros(frame_size, dtype = int)
-print(wafer[:, 0, 0])
-for i in range(dimension_of_frame[0]):
-    for j in range(dimension_of_frame[1]):
-        lower[i, j], upper[i, j] = get_average(wafer[:, i, j])
 
-for i in range(wafer_size[0]):
-    for j in range(frame_size[0]):
-        for k in range(frame_size[1]):
-            #print(lower[j][k], lower[j][k], wafer[i, j, k])
-            if(check_anomalies(wafer[i, j, k], lower[j][k], upper[j][k])):
-                
-                anamoly.append((i, j, k))
-print(lower)
-print(upper)       
+
+#Comparing first and last frame with 2 frames consecutively
+last_idx = wafer_size[0] - 1
+for j in range(0, frame_size[0]):
+        for k in range(0, frame_size[1]):
+            if(abs(wafer[0, j, k] - wafer[1, j, k]) > 4 and abs(wafer[0, j, k] - wafer[2, j, k]) > 4):
+                anamoly.add((1, j, k ))
+            if(abs(wafer[last_idx, j, k] - wafer[last_idx-1, j, k]) > 4 and abs(wafer[last_idx, j, k] - wafer[last_idx-2, j, k]) > 4):
+                anamoly.add((last_idx + 1, j, k))
+
+#Comparing before and previous frame
+for i in range(1, wafer_size[0]-1):
+    for j in range(0, frame_size[0]):
+        for k in range(0, frame_size[1]):
+            if(abs(wafer[i, j, k] - wafer[i-1, j, k]) > 4 and abs(wafer[i, j, k] - wafer[i+1, j, k]) > 4):
+                anamoly.add((i+1, j, k))
+
+
 print(anamoly)
 
-# for i in frame_list:
-#     inst = Die(i.img)
-#     die_list.append(inst)
 
-# for i in frame_list:
-#     wafle = np.concatenate
+
+# # for i in frame_list:
+# #     inst = Die(i.img)
+# #     die_list.append(inst)
+
+# # for i in frame_list:
+# #     wafle = np.concatenate
